af47919c0e0f57491aac6236beb6c0734def1fad
JustChillinBro92 <ayanpurkait11@gmail.com>
2025-12-08 20:33:02 +0530

    user model added

--- src/main/java/com/devs/elib/model/User.java
+++ src/main/java/com/devs/elib/model/User.java
@@ -1,10 +1,12 @@
 package com.devs.elib.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
